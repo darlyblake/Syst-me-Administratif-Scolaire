28f44c53b7e4bd172c63864bdb784f58fcfbf2d3
Darly Blake <darlyblake@gmail.com>
2025-09-05 16:26:23 +0200

    Fix: Configuration pour déploiement GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,9 @@ const nextConfig = {
     unoptimized: true,
   },
   devIndicators : false,
-  
+  basePath: '/Syst-me-Administratif-Scolaire',
+  assetPrefix: '/Syst-me-Administratif-Scolaire',
+  output: 'export',
 }
 
 export default nextConfig
